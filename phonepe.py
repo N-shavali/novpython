8570779776e8bf8c28bea9a0a860c2369e8294ea
N-shavali <shavalinasiruddin9177@gmail.com>
2024-12-17 13:15:12 +0530

    hii

---
+++ phonepe.py
@@ -0,0 +1,15 @@
+while True:
+ print("1 Payment Failed")
+ print("2.Profile and Privacy")
+ print("3.Money Transfer")
+ print("4.Exit")
+ user_inp=int(input("Enter Your Choice :"))
+ if user_inp==1.1:
+   print("Welcome To Payment Failed")
+ elif user_inp==2:
+    print("Welcome To Profile And Privacy")
+ elif user_inp==3:
+    print("Welcome To Money Transfer")
+ elif user_inp==4:
+    print("Exit")
+    break
--- pythonfirstday.py
+++ pythonfirstday.py
@@ -1,5 +1,53 @@
-for x in range(10):
-    if x%2==0:
-        print(x,"Even")
-    else:
-        print(x,"Odd")
+# num=int(input("Enter Number:"))
+# if num<0:
+#     print("-ve number")
+# elif num>0:
+#     print("+ve number")
+# else:
+#     print("number is zero")
+
+
+
+
+# maths=int(input("Enter The Maths Marks :"))
+# Phy=int(input("Enter The Phy Marks :"))
+# Chem=int(input("Enter The Chem Marks :"))
+# total_marks =600
+# sum_marks=maths+Phy+Chem
+# per=(sum_marks/total_marks)*100
+# print(per)
+# if per<35:
+#     print('grade is f')
+# elif per>35 and per<=50:
+#     print('grade is c')
+# elif per in range(51,91):
+#     print('grade is b')
+# elif per in range(91,101):
+#     print('grade is a')
+# else:
+#     print('invalid per')
+
+
+
+# age=int(input("Enter The Age :"))
+# if age in range(18,30):
+#     print("Ticket Is 424.4")
+# elif age in range(31,51):
+#     print("Ticket Price Is 16976.6")
+# elif age>50:
+#     print("No Entry Ticket")
+# else:
+#     print("Enter Valid Age")
+
+
+# name=input("Enter The Name :")
+# exp=int(input(f"enter {name} expereience"))
+# if exp>=1:
+#     if exp>=1 and exp<=5:
+#         print("25k")
+#     elif exp>=6 and exp<=10:
+#         print("50k")
+#     else:
+#         print("75k")
+# else:
+#     print("not allowed"
