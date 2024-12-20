6df1bca4cc188fec0d4f0c34e3fabb13a05f412a
N-shavali <shavalinasiruddin9177@gmail.com>
2024-12-20 08:17:15 +0530

    hii

--- 18dec.py
+++ 18dec.py
@@ -5,14 +5,15 @@
 #     count=count+1
 #     print(count)
 
-count=0
-sum=0
-product=1
-for x in range(1,10,2):
-    print(x)
-    count=count+1
-    sum=sum+x
-    product=product*1
-print('total values of odd',count)
-print('total sum of odd',sum)
-print('total product of odd',product)
+# count=0
+# sum=0
+# product=1
+# for x in range(1,10,2):
+#     print(x)
+#     count=count+1
+#     sum=sum+x
+#     product=product*1
+# print('total values of odd',count)
+# print('total sum of odd',sum)
+# print('total product of odd',product)
+
